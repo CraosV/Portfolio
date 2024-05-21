4c1e12aa5da597a833717511040fcb9c9a08a466
Cristhian Villanueva <villanuevacristhian656@gmail.com>
2024-05-21 16:00:13 -0400

    Add files via upload

--- js/translate.js
+++ js/translate.js
@@ -1,7 +1,8 @@
+
   document.addEventListener('DOMContentLoaded', function() {
     var miModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
     miModal.show();
-  });
+  }); 
 
   var btnSpanish =document.getElementById("btnSpanish");
   var about = document.getElementById("about");
